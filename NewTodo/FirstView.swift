3fd4d82a609f9268aaf1db45b44fd1929f7446d0
Park-Hyun-Bin <gusqls9861@gmail.com>
2023-09-18 23:58:21 +0900

    TodoList에 화면과 UI추가

---
+++ NewTodo/FirstView.swift
@@ -0,0 +1,51 @@
+//
+//  FirstView.swift
+//  NewTodo
+//
+//  Created by t2023-m0049 on 2023/09/16.
+//
+
+import UIKit
+
+class FirstView: UIViewController {
+
+    //MARK: - 여러변수들 선언
+
+    
+    @IBOutlet weak var dateLb: UILabel!
+    // 날짜
+    
+    @IBOutlet weak var dotwLb: UILabel!
+    // 요일
+    
+    @IBOutlet weak var tvListToday: UITableView!
+    // 오늘할일 리스트
+    
+    @IBOutlet weak var btnGoList: UIButton!
+    // 리스트로이동하는 버튼
+    
+    
+    
+    //MARK: - 생명주기 시작
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    //MARK: - 여러 함수
+    
+
+    
+    
+    
+    
+    
+    
+}
+
+
+    //MARK: - 익스텐션 함수
+
---
+++ NewTodo/ListView.swift
@@ -0,0 +1,30 @@
+//
+//  ListView.swift
+//  NewTodo
+//
+//  Created by t2023-m0049 on 2023/09/16.
+//
+
+import UIKit
+
+class ListView: UIViewController {
+
+    //MARK: - 여러변수들 선언
+    
+    @IBOutlet weak var tvList: UITableView!
+    // 리스트
+    
+    @IBOutlet weak var tbCell: UITableViewCell!
+    // 테이블뷰셀
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    
+
+}
